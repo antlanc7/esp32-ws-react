518ab18350bd1a0a9dca1fa9e57db93459893f48
Antonio Lanciotti <antonio.lanciotti@live.it>
2022-12-30 11:17:35 +0100

    Update Temperature.tsx

--- frontend/src/Temperature.tsx
+++ frontend/src/Temperature.tsx
@@ -3,21 +3,15 @@ interface TemperatureProps {
 }
 
 export default function Temperature(props: TemperatureProps) {
-    if (props.value === undefined) {
-        return (
-            <div className="py-1 bg-gray-500">
-                <h1>Temperature: -°C</h1>
-            </div>
-        );
-    }
     const color = (
-        props.value < 18 ? "bg-blue-600" :
-            props.value < 20 ? "bg-emerald-900" :
-                "bg-red-900"
+        props.value === undefined ? "bg-gray-500" :
+            props.value < 18 ? "bg-blue-600" :
+                props.value < 20 ? "bg-emerald-900" : 
+                    "bg-red-900"                  
     );
     return (
         <div className={`py-1 ${color}`}>
-            <h1>Temperature: {props.value}°C</h1>
+            <h1>Temperature: {props.value ?? "-"}°C</h1>
         </div>
     );
 }
